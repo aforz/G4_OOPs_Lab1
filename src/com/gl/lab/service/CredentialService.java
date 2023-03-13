784189ba4d975a3702d5b90e18752716df565bd4
Afroz Alam <afroz3002@gmail.com>
2023-03-13 14:56:08 +0530

    Lab1 initial code added

---
+++ src/com/gl/lab/service/CredentialService.java
@@ -0,0 +1,38 @@
+package com.gl.lab.service;
+
+import com.gl.lab.employee.Employee;
+
+import java.util.Random;
+
+public class CredentialService {
+
+    public final String COMPANY = "abc";
+
+    public String generatePassword() {
+        String password = "";
+        String capLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String smallLetter = "abcdefghijklmnopqrstuvwxyz";
+        String numbers = "0123456789";
+        String symbol = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+        Random random = new Random();
+        password += String.valueOf(capLetter.charAt(random.nextInt(capLetter.length())));
+        password += String.valueOf(capLetter.charAt(random.nextInt(capLetter.length())));
+        password += String.valueOf(smallLetter.charAt(random.nextInt(smallLetter.length())));
+        password += String.valueOf(smallLetter.charAt(random.nextInt(smallLetter.length())));
+        password += String.valueOf(numbers.charAt(random.nextInt(numbers.length())));
+        password += String.valueOf(numbers.charAt(random.nextInt(numbers.length())));
+        password += String.valueOf(symbol.charAt(random.nextInt(symbol.length())));
+        password += String.valueOf(symbol.charAt(random.nextInt(symbol.length())));
+        return password;
+    }
+
+    public String generateEmailAddress(Employee employee, String department) {
+        return employee.getFirstName().toLowerCase() + employee.getLastName().toLowerCase() + "@" + department + "." + COMPANY + ".com";
+    }
+
+    public void showCredentials(Employee employee, String department) {
+        System.out.println("Dear " + employee.getFirstName() + " your generated credentials are as follows");
+        System.out.println("Email ---> " + generateEmailAddress(employee, department));
+        System.out.println("Password ---> " + generatePassword());
+    }
+}
